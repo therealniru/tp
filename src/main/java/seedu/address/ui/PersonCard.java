00141a16f73fdbf4b96f4952441241dad9561362
AK-matrix <arnavkamath2802@gmail.com>
2026-03-30 22:23:02 +0800

    Fix Checkstyle errors

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -12,7 +12,6 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.RejectionReason;
-import seedu.address.model.person.Status;
 
 /**
  * An UI component that displays information of a {@code Person}.
